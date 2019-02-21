792599d86ca774dfa30478f8ec337490b4afd1eb
UlisesCeca <ulises.ceca@dialogotecnologico.com>
2019-02-21 18:54:42 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+declare let cordova;
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,26 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    console.log("hola")
+    const options = {
+      method: 'post',
+      data: { id: 12, message: 'test' },
+      headers: { Authorization: 'OAuth2: token' }
+    };
+
+    cordova.plugin.http.sendRequest('https://google.com/', options, function (response) {
+      // prints 200
+      console.log(response.status);
+    }, function (response) {
+      // prints 403
+      console.log(response.status);
+
+      //prints Permission denied
+      console.log(response.error);
+    });
   }
 
 }
